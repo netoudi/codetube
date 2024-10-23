69bbcf451263a032625d773250aab1ba81adb55d
netoudi <netoudi@rocketmail.com>
2024-10-23 16:23:33 -0300

    chore(studio): config docker

--- studio/videos/settings.py
+++ studio/videos/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'videos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codetube_studio',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'host.docker.internal',
+        'PORT': '5432',
     }
 }
 
